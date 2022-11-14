b2cd458feeb38dc65ae204ec25f114d5227f9a9f
yoelputra1 <118160675+yoelputra1@users.noreply.github.com>
2022-11-14 14:15:53 +0700

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/laravel' => 
+  array (
+    'dont-discover' => 
+    array (
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+);
